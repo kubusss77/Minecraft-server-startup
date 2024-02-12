160a853a9bfcd843c65ff8d201dc06cd03ba27af
Jakub Szczepa <szczepa.jakub07@gmail.com>
2024-02-12 14:53:45 +0100

    Naprawenie wyłączania serwera Minecraft, dodanie późnienia startu.

--- start-mcserver.sh
+++ start-mcserver.sh
@@ -3,10 +3,10 @@
 
 
 # Podstawowa konfiguracja
-serwer_lokalizacja="/mcserver" #Ścieżka do folderu, w którym znajduje się serwer Minecraft
-java_argumenty="@user_jvm_args.txt" #Argumenty javy
-screen_nazwa="mcserver-Stoneblock3" #Nazwa sesji screen, w której działa serwer Minecraft
+java_argumenty="@user_jvm_args.txt @libraries/net/minecraftforge/forge/1.18.2-40.2.14/unix_args.txt nogui" #Argumenty javy
+screen_nazwa="konsolamc" #Nazwa sesji screen, w której działa serwer Minecraft. Musi być inna od nazwy użytkownika uruchamiającego serwer.
 uzytkownik="mcserver" #Użytkownik, na którym działa serwer Minecraft
+opoznienie=20 #Czas, po jakim rozpocznie się uruchamianie serwera Minecraft (javy).
 
 # Zaawansowana konfiguracja
 maks_restartow=5 #Maksymalna ilość restartów serwera Minecraft, po której skrypt zakończy działanie
@@ -14,32 +14,9 @@ czas_restartow=300 #Czas w sekundach, co jaki skrypt będzie sprawdzał, czy ser
 raport_lokalizacja="/tmp/raport/$screen_nazwa" #Ścieżka do pliku, w którym będą zapisywane logi skryptu
 raport="$raport_lokalizacja/raport_$screen_nazwa_$(date '+%Y-%m-%d_%H:%M:%S').txt" #Nazwa pliku, w którym będą zapisywane logi skryptu
 
-
-
-trap 'serwer_wylacz' SIGTERM SIGINT
 mkdir -p "$raport_lokalizacja"
 
-serwer_wlacz() {
-    /usr/local/bin/screen -dmS "$screen_nazwa"
-    /usr/local/bin/screen -S "$screen_nazwa" -X stuff "su -l $uzytkownik -c 'cd \"$serwer_lokalizacja\" && java $java_argumenty' ; exit"
-    /usr/local/bin/screen -S "$screen_nazwa" -X eval "stuff \015"
-    sleep 60
-}
 
-serwer_wylacz() {
-    echo "$(date '+%d-%m-%Y %H:%M:%S') [Info] Otrzymano sygnał zakończenia skryptu. Wyłączanie serwera Minecraft..." | tee -a $raport
-    /usr/local/bin/screen -S "$screen_nazwa" -X stuff "stop"
-    /usr/local/bin/screen -S "$screen_nazwa" -X eval "stuff \015"
-    while true; do
-        sleep 2
-        if serwer_status; then
-            /usr/local/bin/screen -S "$screen_nazwa" -X quit
-            echo "$(date '+%d-%m-%Y %H:%M:%S') [Info] Serwer Minecraft został wyłączony." | tee -a $raport
-            exit 0
-        fi
-    done
-    echo "[Debug] serwer_wylacz koniec" | tee -a $raport
-}
 
 serwer_status() {
     if /usr/local/bin/screen -list | grep -q "$screen_nazwa"; then
@@ -49,14 +26,48 @@ serwer_status() {
     fi
 }
 
+serwer_wlacz() {
+    /usr/local/bin/screen -dmS "$screen_nazwa"
+    /usr/local/bin/screen -S "$screen_nazwa" -X stuff "java $java_argumenty ; exit"
+    /usr/local/bin/screen -S "$screen_nazwa" -X eval "stuff \015"
+    sleep 5
+    if serwer_status; then
+        echo "$(date '+%d-%m-%Y %H:%M:%S') [Info] Uruchamianie serwera Minecraft..." | tee -a $raport
+    else
+        echo "$(date '+%d-%m-%Y %H:%M:%S') [Error] Serwer Minecraft nie został uruchomiony." | tee -a $raport
+        exit 1
+    fi
+}
+
+serwer_wylacz() {
+    echo "$(date '+%d-%m-%Y %H:%M:%S') [Info] Otrzymano sygnał zakończenia skryptu. Wyłączanie serwera Minecraft..." | tee -a $raport
+    if serwer_status; then
+        /usr/local/bin/screen -S "$screen_nazwa" -X stuff "stop"
+        /usr/local/bin/screen -S "$screen_nazwa" -X eval "stuff \015"
+        while true; do
+            sleep 2
+            if ! serwer_status; then
+                echo "$(date '+%d-%m-%Y %H:%M:%S') [Info] Serwer Minecraft został wyłączony." | tee -a $raport
+                exit 0
+            fi
+        done
+    fi
+
+    
+}
+
+
+
+trap 'serwer_wylacz' SIGTERM SIGINT
+echo "$(date '+%d-%m-%Y %H:%M:%S') [Info] Uruchamianie skryptu..." | tee -a $raport
+
 if serwer_status; then
     echo "$(date '+%d-%m-%Y %H:%M:%S') [Info] Serwer Minecraft jest już uruchomiony. Skrypt zostanie zakończony." | tee -a $raport
     exit 1
 else
-    echo "$(date '+%d-%m-%Y %H:%M:%S') [Info] Uruchamianie serwera Minecraft..." | tee -a $raport
+    sleep $opoznienie
     serwer_wlacz
 fi
-
 
 ilosc_restartow=0
 while true; do
